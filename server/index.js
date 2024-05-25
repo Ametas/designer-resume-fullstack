c887c998c931308debd5f154c5ed6c6745f1a196
Ametas <cherrom0708@gmail.com>
2024-05-25 19:13:31 +0300

    changes server/

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 
 const IP = process.env.APP_IP || 'localhost'
 const PORT = process.env.APP_PORT
 
 const app = express()
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'dist')))
+
 const router = require('./routes/resume.routes')
 
 app.use((req, res, next) => {
@@ -13,6 +16,11 @@ app.use((req, res, next) => {
     next()
 })
 app.use('/api', router)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
+})
+
 
 app.listen(PORT, IP, () => {
     console.log(`http://${IP}:${PORT}`);

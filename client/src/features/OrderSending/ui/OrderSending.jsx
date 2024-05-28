8d59fbc310628e5da66e02a1a6b8e6f9bd323b55
Ametas <cherrom0708@gmail.com>
2024-05-28 20:36:32 +0300

    delete cards from last order step

--- client/src/features/OrderSending/ui/OrderSending.jsx
+++ client/src/features/OrderSending/ui/OrderSending.jsx
@@ -10,7 +10,7 @@ export const OrderSending = (props) => {
   <div className={`${style['order-sending']} ${!props.isActive && 'inactive'}`}>
     <FormTitle>Заявка успешно отправлена</FormTitle>
     <FormSubTitle>Приложить примеры желаемого дизайна интерьера?</FormSubTitle>
-    <div className={style["cards-box"]}>
+    {/* <div className={style["cards-box"]}>
       <ChooseCard
         id="add-examples"
         text="Загрузить изображения"
@@ -19,7 +19,7 @@ export const OrderSending = (props) => {
         id="skip-examples"
         text="Пропустить"
       />
-    </div>
+    </div> */}
   </div>
   )
 }
